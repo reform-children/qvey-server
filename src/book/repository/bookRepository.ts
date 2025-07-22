f261c4623ec370bb1ba1f71227d6e2333e5caf49
whisper74541 <zaq4869@naver.com>
2025-07-22 22:32:44 +0900

    updateBook-api 생성

--- src/book/repository/bookRepository.ts
+++ src/book/repository/bookRepository.ts
@@ -1,5 +1,5 @@
 import { BookEntity } from '../entity/bookEntity'
-import { Book, BookListSearchOption, CreateBook, DeleteBook } from '../types/book'
+import { Book, BookListSearchOption, CreateBook, DeleteBook, UpdateBook } from '../types/book'
 import { getPool } from '../../db'
 
 function toBook({ book_description, book_id, book_title, generate_time, modified_time, user_id }: BookEntity): Book {
@@ -18,7 +18,7 @@ export const getAllBook = async (option: BookListSearchOption): Promise<Book[]> 
     let sql = `SELECT * FROM books WHERE 1 = 1`
     const params: any[] = []
     let index = 1
-
+    console.log('SQL:', sql, params);
     if (title) {
         sql += ` AND book_title ILIKE $${index}` // ILIKE는 대소문자 무시
         params.push(`%${title}%`) // %text% 이렇게 들어감
@@ -54,6 +54,26 @@ export const save = async ({ description, title, userId }: CreateBook): Promise<
 export const deleteById = async ({ bookId }: DeleteBook): Promise<void> => {
     try {
         await getPool().query(`DELETE FROM books WHERE book_id = $1`, [bookId])
+    } catch (err) {
+        console.error(err)
+        throw new Error('DB Internal Error')
+    }
+}
+
+export const updateById = async ({ bookId, title, description }: UpdateBook): Promise<void> => {
+    console.log('DB INFO (PATCH):', await getPool().query(`
+        SELECT current_database() db, current_schema() schema, current_user user;
+    `));
+    const sql = `
+        UPDATE books
+        SET
+            book_title       = COALESCE($2, book_title),
+            book_description = COALESCE($3, book_description),
+            modified_time    = NOW()
+        WHERE book_id = $1
+    `
+    try {
+        await getPool().query(sql, [bookId, title ?? null, description ?? null])
     } catch (err) {
         console.error(err)
         throw new Error('DB Internal Error')
