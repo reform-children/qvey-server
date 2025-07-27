a6ac0fc425a4103d780e07d8aa23169f6d41fb79
ptk5758 <ptk725739@gmail.com>
2025-07-27 15:33:51 +0900

    [Fixed] 회원가입 API 수정
    - DTO Model 적절히 변경
    - Email Validate Util 로 이동 및 로직 변경
    - Type 수정

--- src/user/service/userService.ts
+++ src/user/service/userService.ts
@@ -2,39 +2,29 @@
  * 유저 기능 서비스
  */
 
-import { RegistUserRequestDTO, RegisterUserResponseDTO } from '../dto/userDTO'
-import { getUserEmailByEmail, insertUser } from '../repository/userRepository'
-import { RegistUser } from '../type/user'
-import { passwordToHash } from '../utils/utils'
+import { RegisterUser } from '../model/userModel'
+import userRepository from '../repository/userRepository'
+import { passwordToHash, validEmail } from '../utils/utils'
 
 /** 회원가입 서비스 */
-export const registUser = async (data: RegistUser): Promise<RegisterUserResponseDTO> => {
-    const { email, password, name, birth, address, addressDetail, gender, tell } = data
-
+export const registUser = async (data: RegisterUser): Promise<void> => {
+    const { email, password } = data
+    /**
+     * 이메일 검사
+     */
     await validEmail(email)
 
-    const hashedPassword = await passwordToHash(data.password)
+    /**
+     * 중복 검사
+     */
+    const _user = await userRepository.findUserByEmail(email)
+    if (_user) throw new Error('사용 할 수 없는 Email 입니다.')
 
-    const reqData: RegistUserRequestDTO = {
-        ...data,
-        password: hashedPassword,
-    }
+    const hashedPassword = await passwordToHash(password)
 
-    return await insertUser(reqData)
+    await userRepository.save({ ...data, password: hashedPassword })
 }
 
-const validEmail = async (email: string) => {
-    if (!email) {
-        throw new Error('이메일은 필수입니다.')
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    if (!emailRegex.test(email)) {
-        throw new Error('이메일 형식이 올바르지 않습니다.')
-    }
-
-    const existEmailChk: string = await getUserEmailByEmail(email)
-    if (existEmailChk) {
-        throw new Error('이미 사용중인 이메일입니다.')
-    }
+export default {
+    registUser,
 }
